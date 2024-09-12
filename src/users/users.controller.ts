1fc805c8f9eab01cb057b6785b468ede53f528aa
Maxim <zeetreex@gmail.com>
2024-09-12 13:51:00 +0300

    grabbing params, query and body from the request

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
 
 @Controller("users")
 export class UsersController {
-  @Get()
-  public getUsers() {
+  @Get(":id")
+  public getUsers(@Param("id") id: string, @Query() query: any) {
+    console.log(id);
+    console.log(query);
     return "getUsers";
+  }
+
+  @Post()
+  public createUser(@Body() body: any) {
+    console.log(body);
+    return "createUser";
   }
 }
