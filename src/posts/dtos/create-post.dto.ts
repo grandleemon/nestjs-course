d2bfe5aa3503442b918c658aa1a248276ac30657
Maxim <zeetreex@gmail.com>
2024-09-16 22:10:13 +0300

    create post dto validation

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -1,5 +1,17 @@
 import { POST_TYPE } from "../enums/postType.enum";
-import { STATUS } from "../enums/postStatus.enum";
+import { POST_STATUS } from "../enums/postStatus.enum";
+import {
+  IsArray,
+  IsEnum,
+  IsISO8601,
+  IsJSON,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Matches,
+  MinLength,
+} from "class-validator";
 
 interface MetaOption {
   key: string;
@@ -7,14 +19,53 @@ interface MetaOption {
 }
 
 export class CreatePostDto {
+  @IsString()
+  @MinLength(4)
+  @IsNotEmpty()
   title: string;
+
+  @IsEnum(POST_TYPE)
+  @IsNotEmpty()
   postType: POST_TYPE;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+    message:
+      'A slug should be all small letters and uses only "-" and without spaces. For example "my-url"',
+  })
   slug: string;
-  status: STATUS;
+
+  @IsEnum(POST_STATUS)
+  @IsNotEmpty()
+  status: POST_STATUS;
+
+  @IsString()
+  @IsOptional()
   content?: string;
+
+  @IsString()
+  @IsOptional()
+  @IsJSON()
   schema?: string;
+
+  @IsUrl()
+  @IsOptional()
   featuredImageUrl?: string;
-  publishOn: Date;
-  tags: string[];
+
+  @IsISO8601()
+  @IsOptional()
+  publishOn?: Date;
+
+  @IsOptional()
+  @IsArray()
+  @IsString({
+    each: true,
+  })
+  @MinLength(3, {
+    each: true,
+  })
+  tags?: string[];
+
   metaOptions: MetaOption[];
 }
