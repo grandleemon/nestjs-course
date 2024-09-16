39903dca1c96686bdfc96dfb594c7d930e969f95
Maxim <zeetreex@gmail.com>
2024-09-16 14:14:21 +0300

    returning test data from getPosts

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -3,6 +3,15 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class PostsService {
   public findAll(userId: string) {
-    console.log(userId);
+    return [
+      {
+        title: "Title1",
+        content: "Content2",
+      },
+      {
+        title: "Title1",
+        content: "Content2",
+      },
+    ];
   }
 }
