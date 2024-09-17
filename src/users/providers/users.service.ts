1b31d9a42eaa0a871880de273edf14bfdd64ffff
Maxim <zeetreex@gmail.com>
2024-09-17 10:44:18 +0300

    get 100% coverage of the users.service file

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -2,13 +2,22 @@ import { forwardRef, Inject, Injectable } from "@nestjs/common";
 import { GetUsersParamDto } from "../dtos/get-users-param.dto";
 import { AuthService } from "../../auth/providers/auth.service";
 
+/**
+ * Class to connect to Users table and perform business operations
+ */
 @Injectable()
 export class UsersService {
+  /**
+   * Injecting authService
+   */
   constructor(
     @Inject(forwardRef(() => AuthService))
     private readonly authService: AuthService,
   ) {}
 
+  /**
+   * The method to get all users from the database
+   */
   public findAll(
     getUsersParamDto: GetUsersParamDto,
     limit: number,
@@ -26,6 +35,9 @@ export class UsersService {
     ];
   }
 
+  /**
+   * The method to get one specific user by ID from the database
+   */
   public findOneById(id: string) {
     return {
       id: 2,
