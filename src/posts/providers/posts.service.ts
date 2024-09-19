411d063db4c053729eabf455d33cc3a964a890b7
Maxim <zeetreex@gmail.com>
2024-09-19 13:16:59 +0300

    updating the post

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -5,6 +5,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { Post } from "../post.entity";
 import { TagsService } from "../../tags/providers/tags.service";
+import { PatchPostDto } from "../dtos/patch-post.dto";
 
 @Injectable()
 export class PostsService {
@@ -34,6 +35,25 @@ export class PostsService {
       author: user,
       tags,
     });
+
+    return await this.postsRepository.save(post);
+  }
+
+  public async update(patchPostDto: PatchPostDto) {
+    const tags = await this.tagsService.getMultipleTagsById(patchPostDto.tags);
+    const post = await this.postsRepository.findOneBy({
+      id: patchPostDto.id,
+    });
+
+    post.title = patchPostDto.title ?? post.title;
+    post.content = patchPostDto.content ?? post.content;
+    post.status = patchPostDto.status ?? post.status;
+    post.postType = patchPostDto.postType ?? post.postType;
+    post.slug = patchPostDto.slug ?? post.slug;
+    post.featuredImageUrl =
+      patchPostDto.featuredImageUrl ?? post.featuredImageUrl;
+    post.publishOn = patchPostDto.publishOn ?? post.publishOn;
+    post.tags = tags;
 
     return await this.postsRepository.save(post);
   }
