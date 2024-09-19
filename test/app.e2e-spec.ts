07d10ffeb0f9f352cb0bf2540e1de42738913307
Maxim <zeetreex@gmail.com>
2024-09-19 14:38:08 +0300

    checking NODE_ENV when running tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { INestApplication } from "@nestjs/common";
 import * as request from "supertest";
 import { AppModule } from "../src/app.module";
+import * as process from "process";
 
 describe("AppController (e2e)", () => {
   let app: INestApplication;
@@ -16,9 +17,7 @@ describe("AppController (e2e)", () => {
   });
 
   it("/ (GET)", () => {
-    return request(app.getHttpServer())
-      .get("/")
-      .expect(200)
-      .expect("Hello World!");
+    console.log(process.env.NODE_ENV);
+    return request(app.getHttpServer()).get("/").expect(404);
   });
 });
