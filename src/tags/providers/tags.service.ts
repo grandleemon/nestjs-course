3ef159acb96c79c9639d06990a86f93cbba6cb4e
Maxim <zeetreex@gmail.com>
2024-09-19 12:28:24 +0300

    tags service

---
+++ src/tags/providers/tags.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Tag } from "../tag.entity";
+import { Repository } from "typeorm";
+import { CreateTagDto } from "../dtos/create-tag.dto";
+
+@Injectable()
+export class TagsService {
+  constructor(
+    @InjectRepository(Tag)
+    private readonly tagsRepository: Repository<Tag>,
+  ) {}
+
+  public async create(createTagDto: CreateTagDto) {
+    const tag = this.tagsRepository.create(createTagDto);
+
+    return await this.tagsRepository.save(tag);
+  }
+}
--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -2,9 +2,11 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Tag } from "./tag.entity";
 import { TagsController } from "./tags.controller";
+import { TagsService } from "./providers/tags.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Tag])],
   controllers: [TagsController],
+  providers: [TagsService],
 })
 export class TagsModule {}
